d50eaa04e34d36a86fd3764ec91513598ae9645a
edu97DSE <eduardo300497@gmail.com>
2020-08-06 17:12:43 -0500

    Refactoring

--- Source/RomanNumeral.c
+++ Source/RomanNumeral.c
@@ -10,144 +10,27 @@ static char * Append(char *output, const char *toAppend)
 {
    return output + sprintf(output, "%s", toAppend);
 }
+
 void RomanNumeral_Convert(unsigned input, char *output)
 {
-   int i, hundreds, tens, units;
-   i = 0;
-   hundreds = (input / 100) % 10;
-   tens = (input - (hundreds*100)) / 10;
-   units = (input - (hundreds*100 + tens*10));
-   for(i = 0; i <=100; i++)
+   char *CharactersArray []= {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+   unsigned DecimalArray []= {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+   unsigned i, arrayAdress, tableSize;
+
+   for(i = 0; i <= 999; i++)
       {
-         output[i] = '\0';
-      }
-   switch (hundreds)
-      {
-      case 1: 
-         *output = 'C';
-         output++;
-         break;
-      default: 
-         break;
+         output[i] = 0;
       }
 
-   switch (tens)
+   tableSize = sizeof(CharactersArray) / sizeof(char *);
+   for(arrayAdress = 0; arrayAdress < tableSize; arrayAdress++)
       {
-         case 1:
-            *output = 'X';
-            output++;
-            break;
-         case 2:
-            *output = 'X';
-            output++;
-            *output = 'X';
-            output++;
-            break;
-         case 3:
-            *output = 'X';
-            output++;
-            *output = 'X';
-            output++;
-            *output = 'X';
-            output++;
-            break;
-         case 4:
-            *output = 'X';
-            output++;
-            *output = 'L';
-            output++;
-            break;
-         case 5:
-            *output = 'L';
-            output++;
-            break;
-         case 6: 
-            *output = 'L';
-            output++;
-            *output = 'X';
-            output++;
-            break;
-         case 7:
-            *output = 'L';
-            output++;
-            *output = 'X';
-            output++;
-            *output = 'X';
-            output++;
-            break;
-         case 8:
-            *output = 'L';
-            output++;
-            *output = 'X';
-            output++;
-            *output = 'X';
-            output++;
-            *output = 'X';
-            output++;
-            break;
-         case 9:
-            *output = 'X';
-            output++;
-            *output = 'C';
-            output++;
-            break;
-
-         default: 
-            break; 
+         while(input >= DecimalArray[arrayAdress])
+            {
+               output = Append(output, CharactersArray[arrayAdress]);
+               input = (input - DecimalArray[arrayAdress]);
+            }
+         
       }
-
-   switch (units) 
-      {
-         case 1:
-            *output = 'I';
-            break;
-         case 2:
-            *output = 'I';
-            output++;
-            *output = 'I';
-            break;
-         case 3:
-            *output = 'I';
-            output++;
-            *output = 'I';
-            output++;
-            *output = 'I';
-            break;
-         case 4: 
-            *output = 'I';
-            output++;
-            *output = 'V';
-            break;
-         case 5:
-            *output = 'V';
-            break;
-         case 6:
-            *output = 'V';
-            output++;
-            *output = 'I';
-            break;
-         case 7:
-            *output = 'V';
-            output++;
-            *output = 'I';
-            output++;
-            *output = 'I';
-            break;
-         case 8:
-            *output = 'V';
-            output++;
-            *output = 'I';
-            output++;
-            *output = 'I';
-            output++;
-            *output = 'I';
-            break;
-         case 9:
-            *output = 'I';
-            output++;
-            *output = 'X';
-            break;
-         default: 
-            break;
-      }
+   
 }
